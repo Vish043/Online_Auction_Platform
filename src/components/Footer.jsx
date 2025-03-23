965ab609520573ba38ad77b8976b033e48c7ef72
Vish043 <vishalgaonkar2004@gmail.com>
2025-03-23 15:03:18 +0530

    Your commit message here

---
+++ src/components/Footer.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function Home(){
+  return(
+    <div className="footer_cont">
+      <div className="logo_cont footer_logo">
+        <h2><span>Bid</span>Now</h2>
+        <p>2023 BIDNOW AUCTIONS CO.</p>
+      </div>
+      <div className="footer_columns">
+        <h3>Location</h3>
+        <p>Kumbalgodu</p>
+        <p>Bangalore</p>
+        <p>Karnataka</p>
+      </div>
+      <div className="footer_columns">
+        <h3>Quick Links</h3>
+        <p>Home</p>
+        <p>About</p>
+        <p>Bid</p>
+        <p>Login</p>
+      </div>
+      
+    </div>
+  )
+}
